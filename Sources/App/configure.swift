3850ad00b6d56ee2eccf915e2490e259d4e86f10
Kerem Cesme <keremcesme@gmail.com>
2022-08-10 01:00:09 +0300

    asdfasdfasfasfasdfa

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,11 +9,11 @@ public func configure(_ app: Application) throws {
     
     app.databases.use(
         .postgres(
-            hostname: Environment.get("DB_HOST") ?? "localhost",
-            port: Environment.get("DB_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-            username: Environment.get("DB_USERNAME") ?? "vapor_username",
-            password: Environment.get("DB_PASSWORD") ?? "vapor_password",
-            database: Environment.get("DB_NAME") ?? "vapor_database"
+            hostname: Environment.get("DB_HOST") ?? "vnvce-db-free-tier.cri1kmscggha.eu-central-1.rds.amazonaws.com",
+            port: Environment.get("DB_PORT").flatMap(Int.init(_:)) ?? 5432,
+            username: Environment.get("DB_USERNAME") ?? "vnvce_postgres",
+            password: Environment.get("DB_PASSWORD") ?? "Onlykrm-26-HOM-AwS-vnvce-postgres-free-tier",
+            database: Environment.get("DB_NAME") ?? "vnvce_postgres_db_free_tier"
         ),
         as: .psql
     )
